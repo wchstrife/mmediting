43c654197fd8b5b29045010a77744f96eb92cc4e
wchstrife <wch960426@126.com>
2020-12-12 15:41:23 +0800

    for nograd

--- configs/mattors/fba/fba_comp1k_check_full_nograd.py
+++ configs/mattors/fba/fba_comp1k_check_full_nograd.py
@@ -1,4 +1,3 @@
-
 # model settings
 # 1. 去掉fushion
 # 2. 单卡训练
@@ -11,8 +10,8 @@ model = dict(
 
     pretrained='work_dirs/fba/resnet_50_GN_WS_rename.pth',
 
-    loss_alpha_l1=dict(type='L1Loss', loss_weight=1),
-    loss_alpha_comp=dict(type='L1CompositionLoss', loss_weight=1),
+    loss_alpha_l1=dict(type='CharbonnierLoss', loss_weight=1, sample_wise=True),
+    loss_alpha_comp=dict(type='CharbonnierCompLoss', loss_weight=1, sample_wise=True),
     #loss_alpha_grad=dict(type='GradientLoss', loss_weight=1),
     loss_alpha_lap=dict(type='LaplacianLoss', loss_weight=1),
 
